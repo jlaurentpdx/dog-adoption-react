9c7c3637c634962bd0d08a52e3e05588dcd00548
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-05 15:58:43 -0800

    added loader rendering to tests

--- src/views/DogDetail/DogDetail.test.js
+++ src/views/DogDetail/DogDetail.test.js
@@ -4,6 +4,8 @@ import DogDetail from './DogDetail';
 test('DogDetail renders an individual dog with details', async () => {
   const { container } = render(<DogDetail match={{ params: { id: 14 } }} />);
 
+  screen.getByText('...getting ready to introduce...');
+
   await screen.findByText('Eyes');
   await screen.findByText('Gentle giant with eyes that will melt your soul.');
 
