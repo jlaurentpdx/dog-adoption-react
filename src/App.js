59946f4f17e94c737def270e42d92d1eb98bffac
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-05 17:44:32 -0800

    added Error page with route, basic styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/Header/Header';
 import DogList from './views/DogList/DogList';
 import DogDetail from './views/DogDetail/DogDetail';
+import Error from './views/Error/Error';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" component={DogList} />
           <Route path="/dogs/:id" component={DogDetail} />
           <Redirect exact from="/dogs" to="/" />
+          <Route component={Error} />
         </Switch>
       </div>
     </BrowserRouter>
